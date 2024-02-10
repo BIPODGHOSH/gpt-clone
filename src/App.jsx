00c3da10739fb16f0a83585c5c56f045e35c8b76
BIPODGHOSH <bipodghosh0@gmail.com>
2024-02-10 15:20:52 +0530

    adding basic template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" bg-red-300">
+        <h1 className="">hello wrold</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
